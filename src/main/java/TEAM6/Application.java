5cebd1ba8ecca28a7d6056d4748681ff4141c5a3
Ciro Vitiello <ciro.riccione@gmail.com>
2024-03-25 16:46:26 +0100

    added Rates

--- src/main/java/TEAM6/Application.java
+++ src/main/java/TEAM6/Application.java
@@ -3,11 +3,23 @@ package TEAM6;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class Application {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW4");
+public class Application {
+Random random = new Random();
+//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW4");
 
     public static void main(String[] args) {
+        List<Subscription> subscriptionList = new ArrayList<>();
+        for (int i = 0; i <20 ; i++) {
+            Subscription prova = new Subscription(5.99, SubType.randomSubType());
+            subscriptionList.add(prova);
+        }
+        subscriptionList.forEach(System.out::println);
+
 
     }
 }
---
+++ src/main/java/TEAM6/Subscription.java
@@ -0,0 +1,88 @@
+package TEAM6;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+
+public class Subscription extends Rate {
+
+    // ATTRIBUTES
+
+    private long id;
+    private SubType type;
+    private LocalDate startingDate;
+    private LocalDate endingDate;
+    private long duration;
+
+
+    // CONSTRUCTORS
+
+public Subscription(){
+
+}
+    public Subscription(double price, SubType type ) {
+        super(price);
+        this.type = type;
+        setStartingDate();
+        setEndingDate();
+        setDuration();
+    }
+
+    // METHODS
+
+
+    public SubType getType() {
+        return type;
+    }
+
+    public void setType(SubType type) {
+        this.type = type;
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate() {
+        Random random = new Random();
+        if ( this.type == SubType.WEEKLY) {
+
+            this.startingDate = LocalDate.of(2024, random.nextInt(1,7), random.nextInt(1,29));
+        } else {
+            this.startingDate = LocalDate.of(2024, random.nextInt(1,7),1);
+        }
+    }
+
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate() {
+        if ( this.type == SubType.WEEKLY) {
+
+            this.endingDate = this.startingDate.plusDays(7) ;
+        } else { this.endingDate = this.startingDate.plusMonths(1).minusDays(1);
+        }
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration() {
+        this.duration = ChronoUnit.DAYS.between(this.startingDate, this.endingDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Subscription{" +
+                "id=" + id +
+                ", type=" + type +
+                ", startingDate=" + startingDate +
+                ", endingDate=" + endingDate +
+                ", duration=" + duration +
+                ", id=" + id +
+                ", price=" + price +
+                '}';
+    }
+}

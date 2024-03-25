939220bacade0fb54aef047559756cab153e06ca
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-03-25 11:51:20 +0100

    Dependencies added

--- src/main/java/TEAM6/Application.java
+++ src/main/java/TEAM6/Application.java
@@ -1,8 +1,13 @@
 package TEAM6;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW4");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }

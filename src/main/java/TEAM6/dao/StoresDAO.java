0f65dfd8bf0da8e1ba1477731f76cadd9c015834
Ciro Vitiello <ciro.riccione@gmail.com>
2024-03-27 09:47:26 +0100

    added print rates

--- src/main/java/TEAM6/dao/StoresDAO.java
+++ src/main/java/TEAM6/dao/StoresDAO.java
@@ -1,12 +1,17 @@
 package TEAM6.dao;
 
+import TEAM6.entities.Dispenser;
+import TEAM6.entities.Rate;
 import TEAM6.entities.Store;
 
+import TEAM6.entities.Ticket;
 import TEAM6.exceptions.NoFoundException;
+import TEAM6.exceptions.NoRateException;
 import TEAM6.exceptions.NoStoreException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+import java.util.List;
 
 
 public class StoresDAO {
@@ -50,6 +55,30 @@ public class StoresDAO {
 
         } catch (NoStoreException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public void storeList() {
+        List<Store> list = em
+                .createQuery("SELECT s FROM Store s", Store.class).getResultList();
+        list.forEach(System.out::println);
+    }
+
+    //STAMPA BIGLIETTI
+    public void ticketPrint(int storeIntId) {
+        Store storeFound = em.find(Store.class, storeIntId);
+        if (storeFound != null) {
+            Rate ticket = new Ticket();
+            RatesDAO ratesDao = new RatesDAO(em);
+            ratesDao.save(ticket);
+        } else if (storeFound instanceof Dispenser) {
+            Dispenser dispenser = (Dispenser) storeFound;
+            if (!dispenser.getStatus()) {
+                System.out.println("Impossibile emettere il ticket. Distributore non attivo.");
+            }
+        }
+        else {
+            throw new NoRateException(storeIntId);
         }
     }
 

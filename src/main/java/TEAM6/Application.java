c67ebcc51f486713f24b911c112725cdcd2703ca
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-03-28 16:18:47 +0100

    Project completed

--- src/main/java/TEAM6/Application.java
+++ src/main/java/TEAM6/Application.java
@@ -1,11 +1,16 @@
 package TEAM6;
 
 import TEAM6.dao.*;
+import TEAM6.entities.Subscription;
+import TEAM6.entities.Ticket;
+import TEAM6.entities.Transport;
+import TEAM6.enums.TransportStatus;
 import com.github.javafaker.Faker;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
@@ -26,16 +31,6 @@ public class Application {
         RatesDAO ratesDAO = new RatesDAO(em);
         MaintenanceDAO maintenanceDAO = new MaintenanceDAO(em);
         Scanner scanner = new Scanner(System.in);
-
-//        PROVA SUBSCRIPTIONS
-//        List<Subscription> subscriptionList = new ArrayList<>();
-//        for (int i = 0; i <20 ; i++) {
-//            Subscription prova = new Subscription(5.99, SubType.randomSubType());
-//            subscriptionList.add(prova);
-//        }
-//        subscriptionList.forEach(System.out::println);
-
-
         Faker faker = new Faker(Locale.ITALY);
 
 //        ROUTE GENERATOR
@@ -88,25 +83,12 @@ public class Application {
 //            }
 //        }
 
-//        FUNZIONA CONTA TICKET PER STORE E DATA (ERRORI GESTITI)
-//        if (ratesDAO.numberOfTicketsByStoreAndDate(0, 0) != 0) System.out.println(ratesDAO.numberOfTicketsByStoreAndDate(0, 0));
-//        public static void countTransportTravelsOnRoutes(long transportId, TransportDAO transportDAO) {
-//            if (transportDAO.findById(transportId).getTransportStatus() == TransportStatus.ON_SERVICE) {
-//                long numberOfTravels = Math.round(810 / (transportDAO.findById(transportId).getRoute().getAverageTime() * 2));
-//                double realAverageTime = (double) 810 / numberOfTravels;
-//                System.out.println("This transport has been completed his route " + numberOfTravels + " times");
-//                System.out.println("The real average time is about to " + realAverageTime/2 + " minutes");
-//            } else {
-//                System.out.println("This transport is still under maintenance");
-//            }
-//        }
-
         int value;
         do {
             System.out.println("Starting program..");
             System.out.println("Press 0 to close the program!");
             System.out.println("Press 1 to count tickets for store and date!");
-            System.out.println("Press 2 to count subriptions for store and date!");
+            System.out.println("Press 2 to count subscriptions for store and date!");
             System.out.println("Press 3 to count all the rates for store and date!");
             System.out.println("Press 4 to validate a ticket on a transport!");
             System.out.println("Press 5 count how many tickets were validate on 1 specific transport!");
@@ -114,79 +96,270 @@ public class Application {
             System.out.println("Press 7 to verify the subscriptions based on user card!");
             System.out.println("Press 8 count how many times a transport traveled 1 specific route!");
             System.out.println("Press 9 to renew your User card!");
-            value = Integer.parseInt(scanner.nextLine());
+            System.out.println("Press 10 to create a new rate");
+            value = scanner.nextInt();
             switch (value) {
-                case 1:
-                    System.out.println(ratesDAO.numberOfTicketsByStoreAndDate(17, 3));
-                    break;
-                case 2:
 
-                    break;
-                case 3:
+//          FUNZIONA CONTA TICKET PER STORE E DATA (ERRORI GESTITI)
+                case 1: {
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the store Id");
+                                int storeId = scanner.nextInt();
+                                System.out.println("Insert the number of the month to check");
+                                int month = scanner.nextInt();
+                                if (ratesDAO.numberOfTicketsByStoreAndDate(storeId, month) != 0) System.out.println(ratesDAO.numberOfTicketsByStoreAndDate(storeId, month));
+                                break;
+                            }
 
-                    break;
-                case 4:
+                            case 0: {
+                                break;
+                            }
 
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
                     break;
-                case 5:
+                }
 
+//        FUNZIONE CONTA SUBSCRIPTION PER STORE E DATA (ERRORI GESTITI)
+                case 2:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the store Id");
+                                int storeId = scanner.nextInt();
+                                System.out.println("Insert the number of the month to check");
+                                int month = scanner.nextInt();
+                                if (ratesDAO.numberOfSubscriptionsByStoreAndDate(storeId, month) != 0) System.out.println(ratesDAO.numberOfSubscriptionsByStoreAndDate(storeId, month));
+                                break;
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
                     break;
-                case 6:
+                }
 
+                //        FUNZIONA CONTA RATES PER STORE E DATA (ERRORI GESTITI)
+                case 3:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the store Id");
+                                int storeId = scanner.nextInt();
+                                System.out.println("Insert the number of the month to check");
+                                int month = scanner.nextInt();
+                                System.out.println("The number of tickets and subscriptions is: " + ratesDAO.numberOfRatesByStoreAndData(storeId, month));
+                                break;
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
                     break;
-                case 7:
+                }
 
+//        FUNZIONE PER LA VALIDAZIONE DEI TICKET DA UN CERTO MEZZO (ERRORI GESTITI)
+                case 4:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the ticket Id");
+                                int ticketId = scanner.nextInt();
+                                transportDAO.updateTicketStatus(ticketId);
+                                break;
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
                     break;
+                }
 
-                case 0:
+                //        conta quanti ticket sono stati vidimati su mezzo (ERRORI GESTITI)
+                case 5:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the transport Id");
+                                int transportId = scanner.nextInt();
+                                Transport transport = transportDAO.findById(transportId);
+                                if (transport != null) {
+                                    System.out.println(transport.getTicketList().stream().filter(Ticket::isUsed).toList().size());
+                                } else {
+                                    System.out.println("No transport have been found!");
+                                }
+                                break;
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
+                    break;
+                }
+
+//        conta quanti ticket sono stati vidimati in un periodo di tempo (ERRORI GESTITI)
+                case 6:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the month to check");
+                                int month = scanner.nextInt();
+                                if (ratesDAO.countTicketByMonth(month) != 0) System.out.println(ratesDAO.countTicketByMonth(month));
+                                break;
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
+                    break;
+                }
+
+//        FUNZIONE PER LA VERIFICA DEGLI ABBONAMENTI DA UNA TESSERA UTENTE (ERRORI GESTITI)
+                case 7:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the user card number");
+                                int cardNumber = scanner.nextInt();
+                                List<Subscription> subscriptionList = ratesDAO.verifyActiveSubscription(cardNumber);
+                                if (!subscriptionList.isEmpty()) {
+                                    ratesDAO.verifyActiveSubscription(cardNumber).forEach(System.out::println);
+                                } else System.out.println("This user has no active subscriptions");
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
+                    break;
+                }
+
+//        FUNZIONE PER CONTARE QUANTE VOLTE UN MEZZO HA PERCORSO UNA ROTTA (ERRORI GESTITI)
+                case 8:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the transport id");
+                                int transportId = scanner.nextInt();
+                                countTransportTravelsOnRoutes(transportId, transportDAO);
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
+                    break;
+                }
+
+//        FUNZIONE PER AGGIORNARE LA CARD (ERRORI GESTITI)
+                case 9:{
+                    int userControl;
+                    do {
+                        System.out.println("Press 1 to continue or 0 to go back");
+                        userControl = scanner.nextInt();
+                        switch (userControl) {
+                            case 1: {
+                                System.out.println("Insert the user id");
+                                int userId = scanner.nextInt();
+                                userDAO.updateUserCard(userId);
+                            }
+
+                            case 0: {
+                                break;
+                            }
+
+                            default: {
+                                System.out.println("You must press 1 or 0!");
+                            }
+                        }
+                    } while (userControl != 0);
+                    break;
+                }
+
+                case 0:{
                     System.out.println("closing program..");
                     scanner.close();
                     break;
-                default:
+                }
+
+                default: {
                     System.err.println("error! insert the correct value!");
+
+                }
 
             }
         } while (value != 0);
 
-
-//        FUNZIONA CONTA TICKET PER STORE E DATA
-//        System.out.println(ratesDAO.numberOfTicketsByStoreAndDate(8, 2));
-
-//        FUNZIONE CONTA SUBSCRIPTION PER STORE E DATA (ERRORI GESTITI)
-//        System.out.println(ratesDAO.);
-//        if (ratesDAO.numberOfSubscriptionsByStoreAndDate(0, 0) != 0) System.out.println(ratesDAO.numberOfSubscriptionsByStoreAndDate(0, 0));
-
-//        FUNZIONA CONTA RATES PER STORE E DATA (ERRORI GESTITI)
-//        System.out.println("The number of tickets and subscriptions is: " + ratesDAO.numberOfRatesByStoreAndData(0, 0));
-
-//        FUNZIONE PER LA VALIDAZIONE DEI TICKET DA UN CERTO MEZZO (ERRORI GESTITI)
-//        transportDAO.updateTicketStatus(281);
-//        transportDAO.findById(1).getTicketList().forEach(System.out::println);
-
-//        conta quanti ticket sono stati vidimati su mezzo (ERRORI GESTITI)
-//        Transport transport = transportDAO.findById(0);
-//        if (transport != null) {
-//            System.out.println(transport.getTicketList().stream().filter(Ticket::isUsed).toList().size());
-//        } else {
-//            System.out.println("No transport have been found!");
-//        }
-//
-//        conta quanti ticket sono stati vidimati in un periodo di tempo (ERRORI GESTITI)
-//        if (ratesDAO.countTicketByMonth(0) != 0) System.out.println(ratesDAO.countTicketByMonth(0));
-
-//        FUNZIONE PER LA VERIFICA DEGLI ABBONAMENTI DA UNA TESSERA UTENTE (ERRORI GESTITI)
-//        List<Subscription> subscriptionList = ratesDAO.verifyActiveSubscription(1);
-//        if (!subscriptionList.isEmpty()) {
-//            ratesDAO.verifyActiveSubscription(1).forEach(System.out::println);
-//        } else System.out.println("This user has no active subscriptions");
-
-
-//        FUNZIONE PER CONTARE QUANTE VOLTE UN MEZZO HA PERCORSO UNA ROTTA (ERRORI GESTITI)
-//        countTransportTravelsOnRoutes(5, transportDAO);
-
-//        FUNZIONE PER AGGIORNARE LA CARD (ERRORI GESTITI)
-//        userDAO.updateUserCard(40);
-
+        scanner.close();
         em.close();
         emf.close();
     }
